2e3c04e808378fe3c818d28951c9843f752bc5df
MohakJaiswal500101722 <youmissprohit@gmail.com>
2025-02-01 17:06:27 +0530

    day1

---
+++ Add2Numbers.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<stdlib.h>
+#include<string.h>
+#include<string.h>
+#include<math.h>
+#include<vector>
+using namespace std;
+int main() {
+    int a , b;
+    cout << "Enter 1st number : ";
+    cin >> a;
+    cout << "Enter 2nd number : ";
+    cin >> b;
+    int rezult;
+    rezult = a + b;
+    cout << "The sum of " << a << " and " << b << " is : " << rezult << endl;
+
+    return 0;
+}
