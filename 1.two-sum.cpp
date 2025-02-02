f50ac07e014c8526cdbbcfd56a4fdbaa88424282
MohakJaiswal500101722 <youmissprohit@gmail.com>
2025-02-03 01:58:44 +0530

    Bit optimized Brute force approach only

--- 1.two-sum.cpp
+++ 1.two-sum.cpp
@@ -6,16 +6,17 @@
 
 // @lc code=start
 
-//BRUTE FORCE SOLUTION
+// More optimized BRUTE FORCE SOLUTION
 #include <vector>
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         for (int i = 0 ; i < nums.size() ; i++) {
-            for (int j = 0 ; j < nums.size() ; j++) {
-                if (i == j) {
-                    continue;
-                } else if (nums[i] + nums[j] == target) {
+            for (int j = i + 1 ; j < nums.size() ; j++) {
+              //  if (i == j) {
+              //      continue;
+              //  } 
+                if (nums[i] + nums[j] == target) {
                     return {i , j};
                 }
             }
