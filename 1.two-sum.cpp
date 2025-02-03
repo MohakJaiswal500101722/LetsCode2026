277b578f3145f3e30db74b95acedccc9becb78b3
MohakJaiswal500101722 <youmissprohit@gmail.com>
2025-02-03 15:11:27 +0530

    Two Sum Brute Force More Optimised

--- 1.two-sum.cpp
+++ 1.two-sum.cpp
@@ -12,15 +12,13 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         for (int i = 0 ; i < nums.size() ; i++) {
+            int req = target - nums[i]; //calculating the required number
             for (int j = i + 1 ; j < nums.size() ; j++) {
-              //  if (i == j) {
-              //      continue;
-              //  } 
-                if (nums[i] + nums[j] == target) {
+                if (nums[j] == req) {
                     return {i , j};
                 }
             }
-        }
+        } 
         return {};
     }
 };
